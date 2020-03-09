aa3cc11c2a8bf3354594e30e2abcae0e7a782ab3
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-03-09 01:56:37 +0000

    App is built with an initial page and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <navigation/>
+    <lander/>
   </div>
 </template>
 
 <script>
+import Navigation from './components/includes/Navigation.vue'
+import Lander from './components/Lander.vue'
 
 export default {
   name: 'App',
   components: {
+    Navigation,
+    Lander
   }
 }
 </script>
---
+++ src/components/Lander.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <section class="section">
+            <div class="container-fluid">
+                <img id='picofme' alt="Picture of Georgi" class="rounded-circle" src="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/60347534_2423246807720363_5530170401084145664_o.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=Hh-JDsLzP3cAX-cf4x1&_nc_ht=scontent-lht6-1.xx&oh=b748b5f418a07017d48ab39dc2404a3b&oe=5E8D169A">
+                <h1>Georgi Mirchev</h1>
+                <h2>Software Developer</h2>
+                <p>Built with Vue.js and Bootstrap</p>
+                <button type="button" class="btn btn-primary btn-lg">
+                    Contact
+                </button>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Lander'
+}
+</script>
+<style lang="css" scoped>
+    .section{
+        padding-top: 10%;
+        padding-bottom: 10%;
+    }
+    #picofme{
+        width: 250px;
+    }
+</style>
