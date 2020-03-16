21d35455404f5ca416648784b036dab1c9b5e9f0
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-03-16 15:02:13 +0000

    Styled education section

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,58 +1,60 @@
 <template>
-    <div>
-        <section class="education">
-            <h1>Education</h1>
-            <div class="container">
-              <div class="flex">
-                    <div class="title">
-                        <h2>
-                            BSc Computer Science <br>
-                            University of Southampton
-                        </h2>
-                    </div>
-                    <div class="text-right locationdate"> 
-                        <h5>
-                            Southampton, United kingdom
-                        </h5>
-                        <p>
-                           Oct 2015 - Jun 2019
-                        </p>
-                    </div>
-              </div>
-              <div>
-                    <p>
-                        This course opened my eyes to the various possibilities that the
-                        world of computer science has to offer. It sparked my passion for
-                        object oriented programming, game and web development.
-                        Among my most notable achievements are:
-                    </p>
-                    <ul class="achievements text-left">
-                        <li>
-                            Modeling the behavior of fair playout protocols for peer-to-peer
-                            multiplayer games in Event-B (individual project)
-                        </li>
-                        <li>
-                            Learning C# for Unity in under a week
-                        </li>
-                        <li>
-                            Learning enough PHP in order to secure a website in under a
-                            week.
-                        </li>
-                        <li>
-                            Implementing basic search algorithms to solve a tiles puzzle.
-                        </li>
-                    </ul>
-                </div>
-             </div>
-        </section>
-    </div>
+  <div>
+    <section class="education">
+      <h1 class="bg-primary text-white">Education</h1>
+      <div class="container">
+        <div class="flex">
+          <div class="title">
+            <h3 class="bg-light">
+              BSc Computer Science
+              <br />University of Southampton
+            </h3>
+          </div>
+          <div class="text-right locationdate">
+            <h5>Southampton, United kingdom</h5>
+            <p>Oct 2015 - Jun 2019</p>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <p>
+            This course opened my eyes to the various possibilities that the
+            world of computer science has to offer. It sparked my passion for
+            object oriented programming, game and web development. These years
+            gave me the knowledge I needed to start my career as a software / web
+            developer as well as the oportunity to meet and work with like-minded
+            individuals.
+          </p>
+          <hr />
+          <ul class="achievements text-left">
+            <strong>Among my most notable achievements are:</strong>
+            <br />
+            <br />
+            <li>
+              Modeling the behavior of fair playout protocols for peer-to-peer
+              multiplayer games in Event-B (individual project)
+            </li>
+            <li>Learning C# for Unity in under a week</li>
+            <li>
+              Learning enough PHP in order to secure a website in under a
+              week.
+            </li>
+            <li>Implementing basic search algorithms to solve a tiles puzzle.</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <style scoped>
 .education {
-    margin-left: 10%;
-    margin-right: 10%;
+  padding-top: 5%;
+  padding-bottom: 10%;
+}
+.container {
+  padding-top: 25px;
 }
 .locationdate {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
