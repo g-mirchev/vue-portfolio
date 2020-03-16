0c7cb51db68c6901e72418063abd8b4bdce92091
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-03-16 16:34:31 +0000

    Education section made as components and rendered

---
+++ src/components/ECard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="flex">
+      <div class="title">
+        <h3 class="bg-light">
+          {{school.title}}
+          <br />
+          {{school.name}}
+        </h3>
+      </div>
+      <div class="text-right locationdate">
+        <h5>{{school.location}}</h5>
+        <p>{{school.date_started}} - {{school.date_finished}}</p>
+      </div>
+    </div>
+    <hr />
+    <div>
+      <p>{{school.about}}</p>
+      <hr />
+      <ul class="achievements text-left">
+        <strong>Achievements:</strong>
+        <br />
+        <br />
+        <div v-for="achievement in school.achievements" :key="achievement.id">
+          <li>{{achievement.text}}</li>
+        </div>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ECard",
+  props: {
+    school: Object
+  }
+};
+</script>
+<style scoped>
+.locationdate {
+  margin-top: 10px;
+}
+</style>
--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,49 +3,32 @@
     <section class="education">
       <h1 class="bg-primary text-white">Education</h1>
       <div class="container">
-        <div class="flex">
-          <div class="title">
-            <h3 class="bg-light">
-              BSc Computer Science
-              <br />University of Southampton
-            </h3>
-          </div>
-          <div class="text-right locationdate">
-            <h5>Southampton, United Kingdom</h5>
-            <p>Oct 2015 - Jun 2019</p>
-          </div>
-        </div>
-        <hr />
-        <div>
-          <p>
-            This course opened my eyes to the various possibilities that the
-            world of computer science has to offer. It sparked my passion for
-            object oriented programming, game and web development. These years
-            gave me the knowledge I needed to start my career as a software / web
-            developer as well as the oportunity to meet and work with like-minded
-            individuals.
-          </p>
-          <hr />
-          <ul class="achievements text-left">
-            <strong>Among my most notable achievements are:</strong>
-            <br />
-            <br />
-            <li>
-              Modeling the behavior of fair playout protocols for peer-to-peer
-              multiplayer games in Event-B (individual project)
-            </li>
-            <li>Learning C# for Unity in under a week</li>
-            <li>
-              Learning enough PHP in order to secure a website in under a
-              week.
-            </li>
-            <li>Implementing basic search algorithms to solve a tiles puzzle.</li>
-          </ul>
+        <div v-for="school in schools" :key="school.id">
+          <ECard :school="school"></ECard>
         </div>
       </div>
     </section>
   </div>
 </template>
+<script>
+import axios from "axios";
+import ECard from "./ECard";
+
+export default {
+  name: "Education",
+  components: { ECard },
+  data() {
+    return {
+      schools: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/schools.json")
+      .then(response => (this.schools = response.data));
+  }
+};
+</script>
 <style scoped>
 .education {
   padding-top: 5%;
@@ -53,8 +36,5 @@
 }
 .container {
   padding-top: 25px;
-}
-.locationdate {
-  margin-top: 10px;
 }
 </style>
